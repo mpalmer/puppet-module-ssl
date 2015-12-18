c4fa13d48ae8215b16e22d60a79530a33e12e173
Matt Palmer <mpalmer@hezmatt.org>
2015-12-18 21:41:48 +1100

    Support putting certs/keys somewhere other than /etc/ssl

--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -6,20 +6,19 @@
 #
 #     The common name of the certificate you wish to ensure exists.
 #
-define ssl::certificate() {
+define ssl::certificate($destdir = "/etc/ssl") {
 	exec {
 		"create key for ${name}":
-			command => "/usr/bin/openssl genrsa -out /etc/ssl/private/${name}.pem 2048",
-			creates => "/etc/ssl/private/${name}.pem";
+			command => "/usr/bin/openssl genrsa -out ${destdir}/private/${name}.pem 2048",
+			creates => "${destdir}/private/${name}.pem";
 		"create CSR for ${name}":
-			command => "/usr/bin/openssl req -new -key /etc/ssl/private/${name}.pem -subj /CN=${name}/ -out /etc/ssl/${name}.csr",
-			creates => "/etc/ssl/${name}.csr",
-			unless  => "/usr/bin/test -e /etc/ssl/certs/${name}.pem",
+			command => "/usr/bin/openssl req -new -key ${destdir}/private/${name}.pem -subj /CN=${name}/ -out ${destdir}/${name}.csr",
+			creates => "${destdir}/${name}.csr",
+			unless  => "/usr/bin/test -e ${destdir}/certs/${name}.pem",
 			require => Exec["create key for ${name}"];
 		"create self-signed certificate for ${name}":
-			command => "/usr/bin/openssl x509 -req -days 36500 -in /etc/ssl/${name}.csr -signkey /etc/ssl/private/${name}.pem -out /etc/ssl/certs/${name}.pem",
+			command => "/usr/bin/openssl x509 -req -days 36500 -in ${destdir}/${name}.csr -signkey ${destdir}/private/${name}.pem -out ${destdir}/certs/${name}.pem",
 			require => Exec["create CSR for ${name}"],
-			creates => "/etc/ssl/certs/${name}.pem";
+			creates => "${destdir}/certs/${name}.pem";
 	}
 }
-	
