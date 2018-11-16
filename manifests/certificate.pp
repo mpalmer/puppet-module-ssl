499bdb845cf22b7624fd3a8e4e7717c5588bad5f
Matt Palmer <mpalmer@hezmatt.org>
2018-11-16 04:26:22 +0000

    From three commands to one, with no CSR

--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -2,23 +2,15 @@
 #
 # Attributes:
 #
-#  * `name` (*namevar*; required)
+#  * `title` (*namevar*; required)
 #
 #     The common name of the certificate you wish to ensure exists.
 #
 define ssl::certificate($destdir = "/etc/ssl") {
 	exec {
-		"create key for ${name}":
-			command => "/usr/bin/openssl genrsa -out ${destdir}/private/${name}.pem 2048",
-			creates => "${destdir}/private/${name}.pem";
-		"create CSR for ${name}":
-			command => "/usr/bin/openssl req -new -key ${destdir}/private/${name}.pem -subj /CN=${name}/ -out ${destdir}/${name}.csr",
-			creates => "${destdir}/${name}.csr",
-			unless  => "/usr/bin/test -e ${destdir}/certs/${name}.pem",
-			require => Exec["create key for ${name}"];
-		"create self-signed certificate for ${name}":
-			command => "/usr/bin/openssl x509 -req -days 36500 -in ${destdir}/${name}.csr -signkey ${destdir}/private/${name}.pem -out ${destdir}/certs/${name}.pem",
-			require => Exec["create CSR for ${name}"],
-			creates => "${destdir}/certs/${name}.pem";
+		"create self-signed certificate for ${title}":
+			command => "/usr/bin/openssl req -x509 -newkey rsa:2048 -nodes -keyout ${destdir}/private/${title}.pem -subj /CN=${title}/ -out ${destdir}/certs/${title}.pem -days 3650",
+			creates => "${destdir}/private/${title}.pem",
+			unless  => "/usr/bin/test -e ${destdir}/certs/${title}.pem";
 	}
 }
